5fc4abb06c0c6fe90f9f58c419b2b962c1513354
Bhaskar Singh <rougesocket@gmail.com>
2025-05-17 17:30:50 +0530

    removed unused import

--- src/main/java/io/rougesocket/Main.java
+++ src/main/java/io/rougesocket/Main.java
@@ -4,9 +4,6 @@ import io.rougesocket.service.ConvertorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.HashMap;
-
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
--- src/main/java/io/rougesocket/generator/LinkGenerator.java
+++ src/main/java/io/rougesocket/generator/LinkGenerator.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.Properties;
 
 public class LinkGenerator {
